b0709960037ea102dbc65e90cd32b9d60e026f8b
SteveNoyes <stevenmnoyes.af@gmail.com>
2023-01-19 20:50:48 -0500

    script start

---
+++ script.js
@@ -0,0 +1,3 @@
+var issueDesc = document.getElementById('issueDescInput').value;
+var issueSeverity = document.getElementById('issueSeverityInput').value;
+var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
