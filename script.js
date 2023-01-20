7b1339348a68e7a0ec2f94e7c739bc6b54a38691
SteveNoyes <stevenmnoyes.af@gmail.com>
2023-01-19 20:56:20 -0500

    local storage json

--- script.js
+++ script.js
@@ -32,3 +32,16 @@ function saveIssue(e) {
   
   e.preventDefault();
 }
+
+function setStatusClosed(id) {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+  for (var i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues[i].status = 'Closed';
+    }
+  }
+  
+  localStorage.setItem('issues', JSON.stringify(issues));
+  
+  fetchIssues();
+}
