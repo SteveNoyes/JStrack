3b994d64b7f81bcc46b14a834cfac6f17069aa84
SteveNoyes <stevenmnoyes.af@gmail.com>
2023-01-19 20:54:54 -0500

    event listener

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-var issueDesc = document.getElementById('issueDescInput').value;
-var issueSeverity = document.getElementById('issueSeverityInput').value;
-var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+
+function saveIssue(e) {
+  var issueDesc = document.getElementById('issueDescInput').value;
+  var issueSeverity = document.getElementById('issueSeverityInput').value;
+  var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+  var issueId = chance.guid();
+  var issueStatus = 'Open';
+  
+  var issue = {
+    id: issueId,
+    description: issueDesc,
+    severity: issueSeverity,
+    assignedTo: issueAssignedTo,
+    status: issueStatus
+  }
+  
+  if (localStorage.getItem('issues') == null) {
+    var issues = [];
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issues));    
+  }
+  else {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issues));
+  }
+  
+  document.getElementById('issueInputForm').reset();
+  
+  fetchIssues();
+  
+  e.preventDefault();
+}
